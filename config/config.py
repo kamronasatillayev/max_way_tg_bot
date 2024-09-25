3da227fbdba53446017cd478d17a539ceb731a96
Kamronbek2010js <kamronasatillayev93@gmail.com>
2024-09-25 17:47:35 +0500

    birinchi kodlar

---
+++ config/config.py
@@ -0,0 +1,12 @@
+TOKEN = '7418802670:AAGYfLDa5WkwlxekdlHRaS_FghVRTZoUEF0'
+LANG = {
+    'O’zbekcha': 'uz',
+    'Русский': 'ru',
+    'English': 'en'
+}
+ADMIN = 1058730773
+HR = 123131232
+
+
+# Kamron -20
+# Mirshodxo'ja -10
